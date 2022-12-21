dcb75be06353a74c3005172f8e27bb6b102a207b
Aliia <101452567+ceo-of-albeni@users.noreply.github.com>
2022-12-21 09:59:37 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.*;
+import java.io.BufferedReader;
+import java.util.HashMap;
 class Try {
     class Main {
 
@@ -56,6 +59,7 @@ class Try {
             } while (true);
             return "";
         }
+
         // Авторизация директора
         public static void directorInputLgPw() {
             Scanner sc = new Scanner(System.in);
@@ -95,13 +99,14 @@ class Try {
 
                 if (haveInArray == true) {
                     System.out.println("Директор, вы успешно вошли!");
-//                    directorActions();
+                    directorActions();
                     break;
                 } else {
                     System.out.println("Попытайтесь снова");
                 }
             } while (true);
         }
+
         // Авторизация Воспитателя
         public static void mentorInputLgPw() throws IOException {
             Scanner sc = new Scanner(System.in);
@@ -179,4 +184,222 @@ class Try {
                 }
             } while (true);
         }
-    }}
+
+
+//////
+
+    // Действия директора
+    public static void directorActions() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Меню:");
+        System.out.println("(1)Показать список предметов");
+        System.out.println("(2)Показать количество студентов");
+        System.out.println("(3)Показать список учителей");
+        System.out.println("(4)Добавить учителя");
+        System.out.println("(5)Удалить учителя");
+        System.out.println("(6)Добавить детей");
+        System.out.println("(7)Удалить детей");
+        System.out.println("(0)Выход");
+        do {
+            System.out.print("Ваш выбор: ");
+            String chooseAction = sc.nextLine();
+            switch (chooseAction) {
+                case "action1":
+                case "Action1":
+                case "1":
+                    System.out.println("Действие 1");
+                    action1();
+                    directorActions();
+                    break;
+                case "action2":
+                case "Action2":
+                case "2":
+                    System.out.println("Действие 2");
+                    action2();
+                    directorActions();
+                    break;
+                case "action3":
+                case "Action3":
+                case "3":
+                    System.out.println("Действие 3");
+                    action3();
+                    directorActions();
+                    break;
+                case "action4":
+                case "Action4":
+                case "4":
+                    System.out.println("Действие 4");
+                    action4();
+                    directorActions();
+                    break;
+                case "action5":
+                case "Action5":
+                case "5":
+                    System.out.println("Действие 5");
+                    action5();
+                    directorActions();
+                    break;
+                case "action6":
+                case "Action6":
+                case "6":
+                    System.out.println("Действие 6");
+                    action6();
+                    directorActions();
+                    break;
+                case "action7":
+                case "Action7":
+                case "7":
+                    System.out.println("Действие 7");
+                    action7();
+                    directorActions();
+                    break;
+                default:
+                    System.out.println("Такого действия нет в программе!");
+                    directorActions();
+                    break;
+                case "n":
+                    try {
+                        chooseUsers();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                case "action0":
+                case "Action0":
+                case "0":
+                    System.out.println("Выход");
+                    break;
+            }
+            break;
+        } while (true);
+    }
+
+    public static void action1() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            while (br.ready()) {
+                System.out.print(br.readLine() + " ");
+            }
+            System.out.println();
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action2() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            int count = 0;
+            while (br.ready()) {
+                br.readLine();
+                count++;
+            }
+            BufferedReader br1 = new BufferedReader(new FileReader(""));
+            int count1 = 0;
+            while (br1.ready()) {
+                br1.readLine();
+                count1++;
+            }
+            System.out.println("Всего студентов = " + (int) (count + count1));
+            br1.close();
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action3() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            while (br.ready()) {
+                System.out.print(br.readLine() + " ");
+            }
+            System.out.println();
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action4() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ainura_inai\\IdeaProjects\\Sample\\src\\dos.txt"));
+            HashMap<String, Integer> d = new HashMap<>();
+            while (br.ready()) {
+                String line = br.readLine();
+                if (d.containsKey(line)) {
+                    d.put(line, (int) (d.get(line) + 1));
+                } else {
+                    d.put(line, 1);
+                }
+            }
+            System.out.println(d);
+            String s = "dsd";
+            int m = 100;
+            for (String i : d.keySet()) {
+                if (d.get(i) < m) {
+                    m = d.get(i);
+                    s = i;
+                }
+            }
+            System.out.println("Минимальное количество бытовой техники:" + s);
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action5() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ainura_inai\\IdeaProjects\\Sample\\src\\dos.txt"));
+            System.out.print("Доставленная бытовая техника ");
+            int countLine = 0;
+            while (br.ready()) {
+                System.out.print(br.readLine() + ", ");
+                countLine++;
+            }
+            System.out.println("Количество = " + countLine);
+            BufferedReader br1 = new BufferedReader(new FileReader("C:\\Users\\ainura_inai\\IdeaProjects\\Sample\\src\\zak.txt"));
+            System.out.print("Заказанная бытовая техника ");
+            int countline = 0;
+            while (br1.ready()) {
+                System.out.print(br1.readLine() + ", ");
+                countline++;
+            }
+            System.out.println("Количество = " + countline);
+            br1.close();
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action6() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            while (br.ready()) {
+                System.out.print(br.readLine() + " ");
+            }
+            System.out.println();
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void action7() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(""));
+            while (br.ready()) {
+                System.out.print(br.readLine() + " ");
+            }
+            System.out.println();
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}}
